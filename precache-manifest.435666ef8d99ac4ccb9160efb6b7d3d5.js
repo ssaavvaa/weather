566bfa7540c9a4fee587cdf2294d334455499e22
ssaavvaa <Toxasava@icloud.com>
2020-01-10 14:07:47 -0500

    Updates

---
+++ precache-manifest.435666ef8d99ac4ccb9160efb6b7d3d5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fcc87d89d1cd8f39f3482baf43aa945",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "e452f932765eb10a56af",
+    "url": "/weather/static/css/main.c7947626.chunk.css"
+  },
+  {
+    "revision": "53a56c7ca05509a3099e",
+    "url": "/weather/static/js/2.c463a091.chunk.js"
+  },
+  {
+    "revision": "8de4bdbb127f280cc6b79279dd69f244",
+    "url": "/weather/static/js/2.c463a091.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e452f932765eb10a56af",
+    "url": "/weather/static/js/main.ce6f7a6b.chunk.js"
+  },
+  {
+    "revision": "0f4cb34203d5df547e80",
+    "url": "/weather/static/js/runtime-main.a27fdce6.js"
+  },
+  {
+    "revision": "b0f117495d802445c24d4c57d7c39ef3",
+    "url": "/weather/static/media/2.b0f11749.png"
+  },
+  {
+    "revision": "3a6a89e02dc27e3e2a4d8e77994c8422",
+    "url": "/weather/static/media/background.3a6a89e0.png"
+  },
+  {
+    "revision": "f3aa5cf8459f14b0dde1ca278a2473bf",
+    "url": "/weather/static/media/cloudback.f3aa5cf8.png"
+  },
+  {
+    "revision": "a003c2120b83a0b2f704e24e4ddef1e8",
+    "url": "/weather/static/media/computer.a003c212.png"
+  }
+]);
